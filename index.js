b87906fd106085b0e28143aa018fbfd43be3884f
skachy <41330991+skachy@users.noreply.github.com>
2018-07-17 23:09:25 +0200

    update

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
 
+
+var password = "test123";
+var response;
+var entryCount = 0;
+var entryLimit = 3;
+var error = false;
+
+while(response != password && !error){
+     if(entryCount < entryLimit){
+          response = window.prompt("Enter Password");
+          entryCount++;
+     } else {
+          error = true;
+     }
+}
+
+if(error){
+     alert("Too many entries");
+} else {
+     alert("Success");
+}
+
