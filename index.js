c092ef8eaa75d7474f99d702639c588425507a40
skachy <41330991+skachy@users.noreply.github.com>
2018-07-17 15:41:40 +0200

    update

--- index.js
+++ index.js
@@ -1,14 +1,1 @@
 
-
-function greet(name, lastName) {
-    console.log('Hello ' + name + ' ' + lastName);
-}
-
-
-function square(number) {
-    return number * number;
-}
-
-
-console.log(square(2));
-
