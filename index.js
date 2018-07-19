ea5b52a65af8f534350a12eca4c4e19aa1972cef
skachy <41330991+skachy@users.noreply.github.com>
2018-07-19 21:13:35 +0200

    up

--- index.js
+++ index.js
@@ -1,5 +1,22 @@
 
+/*
+
+
 var name = window.prompt("what is your name?");
 var age = window.prompt("How old are you?");
 document.write('Hey ' + name + ', How are you today? Do you like being ' + age + ' years')
+
+*/
+
+var vekt = window.prompt('VEKT (I KILO)')
+var høyde = window.prompt('HØYDE (I METER)')
+
+var bmi = vekt / (høyde * høyde)
+
+alert(bmi)
+
+
+
+
+
 
